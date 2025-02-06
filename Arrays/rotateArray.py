1e981443fe844d5c98e56a5e7849925d214dbbca
kundan kumar <kkumar@iastate.edu>
2025-02-06 11:25:21 -0600

    Update rotateArray.py

--- Arrays/rotateArray.py
+++ Arrays/rotateArray.py
@@ -1,5 +1,8 @@
 """Given an integer array nums, rotate the array to the right by k steps, where k is non-negative."""
 
+"""
+Method 1: Time Complexity : O(n^2), Space : O(1)
+"""
 class Solution(object):
     def rotate_array(self,nums,k):
         k %= len(nums)
@@ -12,6 +15,23 @@ class Solution(object):
                 # print(prev)
         return nums
 
+"""
+Method 2: Time Complexity : O(n), Space : O(n)
+"""
+
+class Solution(object):
+    def rotate_array(self,nums,k):
+
+        n = len(nums)
+        arr = [0]*n
+
+        for i in range(n):
+            arr[(i+k)%n] = nums[i]
+
+        nums[:] = arr
+
+        return nums
 
 print(Solution().rotate_array([1,2,3,4,5,6,7],3))
 print(Solution().rotate_array([-1,-100,3,99],2))
+
