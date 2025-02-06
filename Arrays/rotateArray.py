174e6af6003f1b0fb6ee008cae7ec80945e8e90d
kundan kumar <kkumar@iastate.edu>
2025-02-06 10:34:53 -0600

    Create rotateArray.py

---
+++ Arrays/rotateArray.py
@@ -0,0 +1,16 @@
+"""Given an integer array nums, rotate the array to the right by k steps, where k is non-negative."""
+
+class Solution(object):
+    def rotate_array(self,nums,k):
+        k %= len(nums)
+        print(k)
+        for i in range(k):
+            prev = nums[-1]
+            for j in range(len(nums)):
+                nums[j], prev = prev,nums[j]
+                # print(nums[j])
+                # print(prev)
+        return nums
+
+
+print(Solution().rotate_array([1,2,3,4,5,6,7],3))
