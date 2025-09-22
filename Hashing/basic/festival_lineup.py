833fa66ebf5a83fdcb15f3a71f078bda42401977
kundan kumar <cs.kundann@gmail.com>
2025-09-22 10:23:25 -0500

    update!!!

---
+++ Hashing/basic/festival_lineup.py
@@ -0,0 +1,9 @@
+"""
+1. Sort the array
+2. Keep track of current length count (current) and longest length count (longest). Both starting at 1.
+3. Loop through the sorted array 
+4. Skip duplicates
+5. If current number is one more than previous number, increment current streak 
+6. Else set longest streak and reset current streak
+7. Return the longest streak
+"""
