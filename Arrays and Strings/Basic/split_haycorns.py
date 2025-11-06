042291a737514a001b39ae719b01ea1bcaef1631
kundan kumar <kkundan7kumar@gmail.com>
2025-11-06 11:11:03 -0600

    feat: #100DaysofCode Day#1

---
+++ Arrays and Strings/Basic/split_haycorns.py
@@ -0,0 +1,16 @@
+"""
+Piglet's has collected a big pile of his favorite food, haycorns, and wants to split them evenly amongst his friends. 
+Write a function split_haycorns() to help Piglet determine the number of ways he can split his haycorns into even groups. 
+split_haycorns() accepts a positive integer quantity as a parameter and returns a list of all divisors of quantity.
+"""
+
+def split_haycorns(quantity):
+    return s
+
+
+
+quantity = 6
+print(split_haycorns(quantity))
+
+quantity = 1
+print(split_haycorns(quantity))
